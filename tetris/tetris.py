6a3c9339651a04e5d2dbabb1d8e94e06f1df7f31
youssef <xyoussefx1500@gmail.com>
2025-04-04 01:34:56 +0200

    restarting

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -1,170 +1,108 @@
-import time
-import copy
 from blessed import Terminal
-from tetrimino import Tetrimino
-            
+import time
+from tetrimino import Tetrimino as Tet
+
 term = Terminal()
 
-# init grid
-grid = [[None for _ in range(10)] for _ in range(20)]
+FPS = 0.5     # 10 frames rendered per second  
 WIDTH = 10
 HEIGHT = 20
-FALL_SPEED = 1
-score = 0
 
-def render_grid(piece):
-    print(term.clear()) 
-    # render grid
-    for i in range(HEIGHT): 
-        for j in range(WIDTH):
-            if grid[i][j]:
-                print(term.move_xy(j*2, i) + grid[i][j]("██"), end="")
-            else:
-                print(term.move_xy(j*2, i) + "0", end="")
-    # render current current_piece
-    for i in range (len(piece.shape)):
-        for j in range(len(piece.shape[0])):
-            if piece.shape[i][j] == 1:
-                print(term.move_xy((piece.x + j) * 2, piece.y + i) + piece.color("██"))
+grid = [[None for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+def graduations():
+    # vertical
+    for i in range(len(grid)):
+        print(term.move_xy(22, i) + str(i))
+    # horizontal
+    for i in range(len(grid[0])):
+        print(term.move_xy(i * 2, 22) + str(i))
     return
 
-def is_valid_move(piece, dx, dy):
-    # line
+def render_grid():
+    print(term.clear())
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                print(term.move_xy(j * 2, i) + grid[i][j]("1"))
+            else:
+                print(term.move_xy(j * 2, i) + ".")
+    return
+
+def render_piece(piece):
     for i in range(len(piece.shape)):
-        # cell
         for j in range(len(piece.shape[0])):
             if piece.shape[i][j]:
-                # simulate next move
-                new_x = piece.x + j + dx
-                new_y = piece.y + i + dy
-                if new_x < 0 or new_x >= WIDTH or new_y < 0 or new_y >= HEIGHT:
-                    return False
-                if grid[new_y][new_x]:
-                    return False
-    return True
-
-def is_valid_rotation(piece):
-    # create copy, due to pass by ref
-    clone = copy.deepcopy(piece)
-    clone.rotate()
-    # lines
-    for i in range(len(clone.shape)):
-        # cells
-        for j in range(len(clone.shape[0])):
-            # if cell active
-            if clone.shape[i][j] == 1:
-                # simulate rotation
-                new_x = clone.x + j
-                new_y = clone.y + i
-                # out of bounds collision
-                if new_x < 0 or new_x >= WIDTH or new_y < 0 or new_y >= HEIGHT:
-                    return False
-
-                # if collision return false
-                if grid[new_y][new_x]:
-                    return False
-    return True
+                print(term.move_xy((piece.x + j) * 2, piece.y + i) + piece.color("1"))
+    return
 
 def is_piece_placed(piece):
-    # loop over lines
-    for i in range (len(piece.shape)):
-        # loop over cells
+    msg = f"current position: ({str(piece.x)},{str(piece.y)})"
+    debugger(msg, 30, 2)
+    for i in range(len(piece.shape)):
         for j in range(len(piece.shape[0])):
-            # if not null
-            if piece.shape[i][j] == 1:
-                # simulate next position
+            if piece.shape[i][j]:
                 new_x = piece.x + j
+                # simulate down by one
                 new_y = piece.y + i + 1
+                msg = f"new position: ({str(new_x)},{str(new_y)})"
+                debugger(msg, 30, 3)
                 if new_y >= HEIGHT:
                     return True
                 if grid[new_y][new_x]:
                     return True
     return False
 
+def is_valid_move(piece, dx, dy):
+    # write this function probab tomorrow
+    return True
+
+def debugger(msg, x, y):
+    print(term.move_xy(x, y) + msg)
+    return
+
 def save_to_grid(piece):
-    for i in range (len(piece.shape)):
+    for i in range(len(piece.shape)):
         for j in range(len(piece.shape[0])):
-            if piece.shape[i][j] == 1:
+            if piece.shape[i][j]:
                 pos_x = piece.x + j
                 pos_y = piece.y + i
-                if 0 <= pos_x < WIDTH and 0 <= pos_y < HEIGHT:
-                    grid[pos_y][pos_x] = piece.color
+                grid[pos_y][pos_x] = piece.color
+    msg = f"saved position: ({str(piece.x)},{str(piece.y)})"
+    debugger(msg, 30, 4)
     return
 
-def place_piece(piece):
-    while (not is_piece_placed(piece)) :
-        piece.move(0, 1)
-    save_to_grid(piece)
-    return
-
-def check_full_lines():
-    for i, line in enumerate(grid):
-        line_complete = True
-        for cell in line:
-            if not cell:
-                line_complete = False
-                break
-        if line_complete:
-            # empty the line
-            for j in range(len(grid[0])):
-                grid[i][j] = None
-            # shift downwards by 1
-            drop_by_one(i)
-    return
-
-def drop_by_one(index):
-    for i in range(index, 0, -1):
-        for j in range(len(grid[0])):
-            grid[i][j] = grid[i - 1][j]
-    for i in range(len(grid[0])):
-        grid[0][i] = None
-    return
-
-with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-    current_piece = Tetrimino.random_tetrimino()
-    last_fall_time = time.monotonic()
+with term.cbreak(), term.fullscreen(), term.hidden_cursor():
+    init_time = time.monotonic()
+    current_piece = Tet.random_tetrimino()
 
     while True:
-        render_grid(current_piece)
-        check_full_lines()
-
-        key = term.inkey(timeout=0.05)
-        
-        if key == 'q':
-            break
-
-        elif key == 'h':
-            # go left
-            if is_valid_move(current_piece, -1, 0):
-                current_piece.move(-1, 0)
-
-        elif key == 'l':
-            # go right
-            if is_valid_move(current_piece, 1, 0):
-                current_piece.move(1, 0)
-
-        elif key == 'j':
-            # go down
-            if is_valid_move(current_piece, 0, 1):
-                current_piece.move(0, 1)
-
-        elif key == 'k':
-            # rotate
-            if is_valid_rotation(current_piece):
-               current_piece.rotate()
-
-        elif key == ' ':
-            # todo right place piece fn
-            place_piece(current_piece)
-
         current_time = time.monotonic()
+        if current_time - init_time > FPS:
+            init_time = time.monotonic()
+            render_grid()
+            render_piece(current_piece)
+            graduations()
+            #current_piece.move(0, 1)
+            if is_piece_placed(current_piece):
+                save_to_grid(current_piece) 
+                current_piece = Tet.random_tetrimino()
 
-        if(is_piece_placed(current_piece)):
-            save_to_grid(current_piece)
-            current_piece = Tetrimino.random_tetrimino()
-
-        if (current_time - last_fall_time > FALL_SPEED):
-            current_piece.move(0, 1)
-            last_fall_time = current_time 
-
+            # controller hjkli
+            key = term.inkey()
+            if key == 'h':
+                # go left
+                current_piece.move(-1, 0)
+            elif key == 'j':
+                # go down
+                current_piece.move(0, 1)
+            elif key == 'k':
+                # go up
+                current_piece.move(0, -1)
+            elif key == 'l':
+                # go right 
+                current_piece.move(1, 0)
+            elif key == ' ':
+                current_piece.rotate()
 
